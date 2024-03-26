ac9d4712625845bfd3c5d738a89f32b1eb38a968
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-03-26 14:46:27 +0000

    feat:avatar

---
+++ src/Component/Avatar.jsx
@@ -0,0 +1,12 @@
+import { Avatar } from "@material-tailwind/react";
+
+export default function AvatarVariants() {
+  return (
+    <div className="flex gap-4">
+      <Avatar
+        src="https://media.licdn.com/dms/image/D4E03AQH-JPLJwze1bQ/profile-displayphoto-shrink_400_400/0/1697302022560?e=1717027200&v=beta&t=koSDqWtj20vHResvm0z2rFuIdoC6EMautlSd-CFd9NE"
+        alt="avatar"
+      />
+    </div>
+  );
+}
