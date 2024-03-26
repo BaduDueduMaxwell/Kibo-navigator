0390f9c0595e7c7ddb85df45cb7d0c41181bac82
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-03-26 14:57:37 +0000

    feat:routing of pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,25 @@ import React from "react";
 import Login from "./Component/Login";
 import Payment from "./Component/Payment";
 import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
 
-export default function App() {
+const isLoggedIn = true; // Replace this with your actual authentication logic
+
+function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="payment" element={<Payment />} />
-    </Routes>
+    <div>
+      {isLoggedIn} {/* Conditionally render Navbar based on login status */}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        {isLoggedIn && (
+          <>
+            <Route path="home" element={<Home />} />
+            <Route path="payment" element={<Payment />} />
+          </>
+        )}
+      </Routes>
+    </div>
   );
 }
+
+export default App;
