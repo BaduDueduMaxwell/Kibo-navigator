edc54d0f45f43f53f5445b2386747d4fede09109
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-03-26 09:17:37 +0000

    feat:routing of payment.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Login from "./Component/Login";
+import Payment from "./Component/Payment";
 import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="payment" element={<Payment />} />
     </Routes>
   );
 }
